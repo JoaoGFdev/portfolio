35962c866de6fd1f190e5744d04544eca855d0eb
joaogf <me@joaogf.dev>
2025-07-16 17:14:46 -0300

    feat: update eslint-plugin-simple-import-sort to version 12.1.1
    - Added simple-import-sort plugin to ESLint configuration
    - Enabled sorting rules for imports and exports

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import js from "@eslint/js"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
+// Plugins
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -21,6 +24,9 @@ export default [
       parserOptions: {
         project: true,
       },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       "@typescript-eslint/array-type": "off",
@@ -47,8 +53,8 @@ export default [
           },
         },
       ],
-      // "simple-import-sort/imports": "error",
-      // "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
       "import/first": "error",
       "import/newline-after-import": "error",
       "import/no-duplicates": "error",
